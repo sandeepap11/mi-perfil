79af32bdfd96e28fc6e87db3f055fba1f21a59ff
sandeepramachandra <sandeepap11@gmail.com>
2020-06-24 01:03:51 +0530

    feat: Add brand icons

--- mi-perfil/src/components/profile/about/About.tsx
+++ mi-perfil/src/components/profile/about/About.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import "../../../styles/profile/About.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function About() {
   return (
@@ -11,7 +14,17 @@ export default function About() {
           I am a Front-End developer specializing in React JS.
         </p>
       </div>
-      <div className="profile-about-social"></div>
+      <div className="profile-about-social">
+        <a target="blank" href="https://twitter.com/sandeep_madavu">
+          <FontAwesomeIcon icon={faTwitter as any} />
+        </a>
+        <a target="blank" href="https://github.com/sandeepap11">
+          <FontAwesomeIcon icon={faGithub as any} />
+        </a>
+        <a href="mailto:webmaster@example.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </div>
     </div>
   );
 }
