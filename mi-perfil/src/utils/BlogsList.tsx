e6b1d287d5c3e68efde7fef120143efd64958b60
sandeepramachandra <sandeepap11@gmail.com>
2020-07-09 00:08:42 +0530

    fix: restore blog list

--- mi-perfil/src/utils/BlogsList.tsx
+++ mi-perfil/src/utils/BlogsList.tsx
@@ -2,22 +2,22 @@ export const blogList = [
   {
     id: 1,
     title: "A Custom React Grid - Part 1",
-    subtitle: "List fro&m API and display",
+    subtitle: "List from API and display",
     date: "July 07 2020",
     tags: ["react", "grid", "table", "api"],
-    author: "Sande/ep Madavu"
+    author: "Sandeep Madavu"
   },
   {
     id: 2,
-    title: "A Custom Rea&ct Grid - Part 2",
+    title: "A Custom React Grid - Part 2",
     subtitle: "Pagination",
     date: "July 10 2020",
     tags: ["react", "grid", "table", "pagination"],
-    author: "Sande/ep Madavu"
+    author: "Sandeep Madavu"
   },
   {
     id: 3,
-    title: "A Custom Rea&ct Grid - Part 3",
+    title: "A Custom React Grid - Part 3",
     subtitle: "Search and Sort",
     date: "July 15 2020",
     tags: ["react", "grid", "table", "search", "sort"],
