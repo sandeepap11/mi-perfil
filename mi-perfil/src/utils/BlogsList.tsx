70cdabc4dfd01db46de37c42f9cb51aaf493e078
sandeepramachandra <sandeepap11@gmail.com>
2020-08-15 23:49:13 +0530

    blog 2 changes

--- mi-perfil/src/utils/BlogsList.tsx
+++ mi-perfil/src/utils/BlogsList.tsx
@@ -251,15 +251,22 @@ export const blogList: Blog[] = [
         id: 2,
         type: "p",
         data: [
-          "This is part 2 of the custom React Grid series. In the first part, we read the table data from the api and then loaded it on to the grid. In this one, we will add dynamic pagination. The code for this part is available ",
+          "In the previous section, we loaded data to our table from the api and displayed it. Now, we need to add dynamic pagination as we do not want to show all the data in one page. The code for this part is available ",
           "https://github.com/sandeepap11/example-code/tree/gridseries-blog-2",
           ". So, let's get to it!"
         ]
       },
       {
         id: 3,
+        type: "p",
+        data: [
+          "Firstly we will define max rows per page as 10 and introduce a pageNumber state in our main component. Then we will calculate the total pages and the 10 games that should be shown based on the page number as below."
+        ]
+      },
+      {
+        id: 4,
         type: "code",
-        header: "GridRow.js",
+        header: "GridMain.js snippet",
         data: [
           "const ROWS_PER_PAGE = 10;",
           "...",
@@ -271,6 +278,227 @@ export const blogList: Blog[] = [
           "    (pageNumber - 1) * ROWS_PER_PAGE,",
           "    pageNumber * ROWS_PER_PAGE",
           "  );"
+        ]
+      },
+      {
+        id: 5,
+        type: "p",
+        data: [
+          "Now, if the last page has less than 10 rows then our table will reduce in height moving all the other componnets below it (if there are other components). To prevent this, we will fill the table with empty rows to keep the table height intact."
+        ]
+      },
+      {
+        id: 6,
+        type: "code",
+        header: "Grid.js snippet",
+        data: [
+          "  let emptyRows = [];",
+          "",
+          "if (rowsPerPage > matches.length) {",
+          "  for (let i = 0; i < rowsPerPage - matches.length; i++) {",
+          "    emptyRows.push(i);",
+          "  }",
+          "}",
+          "...",
+          "",
+          "{rowsPerPage > matches.length &&",
+          "  emptyRows.map(emptyRow => (",
+          `    <div key={emptyRow} className="grid-row-empty"></div>`,
+          "  ))}"
+        ]
+      },
+      {
+        id: 7,
+        type: "p",
+        data: [
+          "All that is left now is to define a reusable pagination component which we can place below our Grid component. This will have 4 buttons for step to first page, step to previous page, step to next page and step to last page. We will also provide an option to provide the page number manually, and display the current rows status (e.g., 2-20 rows of 52). ",
+          "The said component will be independent of the Grid, it's values, column count, maximum row count, api, etc. To enable that it'll need the below props."
+        ]
+      },
+      {
+        id: 8,
+        type: "p",
+        data: [
+          "rowsPerPage: Maximum allowed number of rows. This will help in showing the display status."
+        ]
+      },
+      {
+        id: 9,
+        type: "p",
+        data: ["rowsInCurrentPage: Total available rows rto be shown."]
+      },
+      {
+        id: 10,
+        type: "p",
+        data: [
+          "totalRows: Total rows returned by the API. If you have a grid which will need to make separate api calls for every page, then this will be the total rows in the system."
+        ]
+      },
+      {
+        id: 11,
+        type: "p",
+        data: ["totalPages: Total pages of data."]
+      },
+      {
+        id: 12,
+        type: "p",
+        data: ["pageNumber: Current Page Number."]
+      },
+      {
+        id: 13,
+        type: "p",
+        data: [
+          "setPageNumber: A setter for Page Number whenever it is modified."
+        ]
+      },
+      {
+        id: 14,
+        type: "p",
+        data: [
+          "In the Pagination Component, let's define a reusable component for the pagination buttons. This will take an icon, a method to fire on click and a boolean flag to say whether the button is disabled, as props. We wil use FontAwesome icons."
+        ]
+      },
+      {
+        id: 15,
+        type: "code",
+        header: "Pagination.js Snippet",
+        data: [
+          "const PaginationControl = ({ icon, onClick, isDisabled }) => {",
+          "  return (",
+          "    <div",
+          "      className={",
+          "        isDisabled",
+          `          ? "pagination-page-control-disabled"`,
+          `          : "pagination-page-control"`,
+          "      }",
+          "      onClick={() => {",
+          "        if (!isDisabled) onClick();",
+          "      }}",
+          "    >",
+          "      <FontAwesomeIcon icon={icon} />",
+          "    </div>",
+          "  );",
+          "};"
+        ]
+      },
+      {
+        id: 16,
+        type: "p",
+        data: [
+          "As discussed, the Pagination Component will have 4 buttons for navigation, a textbox to go directly to a page and a display stating the current rows on display. It will look something like below."
+        ]
+      },
+      { id: 17, type: "image", data: ["grid-series-2-pagination-strip.png"] },
+      {
+        id: 18,
+        type: "p",
+        data: [
+          "Before we look at the presentational JSX, we will write a method to update the page number as and when the buttons are pressed or the textbox is given a value. The page number will get passed as a parameter. Note that we will maintain a local state for page number as well. This is mainly to hold the page number value from text box before the user hits the return key. In addition, we will set up a useEffect hook to update the page number from local state as and when the page number prop itself is updated from the parent."
+        ]
+      },
+      {
+        id: 19,
+        type: "code",
+        header: "Pagination.js Snippet",
+        data: [
+          "...",
+          "const [currentPageNumber, setNewCurrentPageNumber] = useState(pageNumber);",
+          "",
+          "useEffect(() => setNewCurrentPageNumber(pageNumber), [pageNumber]);",
+          "",
+          "  const updatePageNumber = currentPageNumberValue => {",
+          "    if (",
+          "      isNaN(currentPageNumberValue) ||",
+          "      Number(currentPageNumberValue) > totalPages ||",
+          "      Number(currentPageNumberValue) < 1",
+          "    ) {",
+          "      setNewCurrentPageNumber(pageNumber);",
+          "    } else {",
+          "      setNewCurrentPageNumber(Number(currentPageNumberValue));",
+          "      setPageNumber(Number(currentPageNumberValue));",
+          "    }",
+          "  };",
+          "..."
+        ]
+      },
+      {
+        id: 20,
+        type: "p",
+        data: [
+          "With all this set, we can now write our JSX as below. For the textbox, we will have an onchange handler as well as an onblur handler. If the return key is pressed, we will fire the blur event and hence updating the page number as well. You may change this logic as per your needs. Also note that if a number lower than 1 or a number more than the number of pages is entered, we simply ignore it."
+        ]
+      },
+      {
+        id: 21,
+        type: "code",
+        header: "Pagination.js Snippet",
+        data: [
+          "  return (",
+          `    <div className="pagination-main">`,
+          `      <div className="pagination-control">`,
+          "        <PaginationControl",
+          "          icon={faStepBackward}",
+          "          onClick={() => updatePageNumber(1)}",
+          "          isDisabled={pageNumber === 1}",
+          "        />",
+          "        <PaginationControl",
+          "          icon={faBackward}",
+          "          onClick={() => updatePageNumber(pageNumber - 1)}",
+          "          isDisabled={pageNumber === 1}",
+          "        />",
+          `        <div className="pagination-page-input-control">`,
+          `          <div className="pagination-page-control">`,
+          "            <input",
+          `              type="text"`,
+          `              className="pagination-page-input"`,
+          "              onChange={event => setNewCurrentPageNumber(event.target.value)}",
+          "              onBlur={() => updatePageNumber(currentPageNumber)}",
+          "              onKeyDown={event => {",
+          "                if (event.keyCode === 13) { //on return key press",
+          "                  event.target.blur();",
+          "                }",
+          "              }}",
+          "              value={currentPageNumber}",
+          "            />",
+          "          </div>",
+          `          <div className="pagination-page-control pagination-page-input">`,
+          `            {" "}`,
+          "            / {totalPages}",
+          "          </div>",
+          "        </div>",
+          "        <PaginationControl",
+          "          icon={faForward}",
+          "          onClick={() => updatePageNumber(pageNumber + 1)}",
+          "          isDisabled={pageNumber === totalPages}",
+          "        />",
+          "        <PaginationControl",
+          "          icon={faStepForward}",
+          "          onClick={() => updatePageNumber(totalPages)}",
+          "          isDisabled={pageNumber === totalPages}",
+          "        />",
+          "      </div>",
+          `      <div className="pagination-info">`,
+          `        {" "}`,
+          `        {(pageNumber - 1) * rowsPerPage + 1} -{" "}`,
+          "        {(pageNumber - 1) * rowsPerPage + rowsInCurrentPage} rows of {totalRows}",
+          "      </div>",
+          "    </div>",
+          "  );"
+        ]
+      },
+      {
+        id: 22,
+        type: "p",
+        data: [
+          "With this, we should get a screen like below with pagination working. If you have an api that fetches data for every page, then you should include that as well in your setPageNumber prop passed from the main Grid component."
+        ]
+      },
+      { id: 23, type: "image", data: ["grid-series-2.png"] },
+      {
+        id: 24,
+        type: "post",
+        data: [
+          "That concludes the pagination changes. Up next we will add search and sort in the next post. Ciao!"
         ]
       }
     ]
